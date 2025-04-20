95d356d671fabdca937a40ffa9bc48efbcaaae1b
ItsMikhaeli <josephmikhaeli.jalandoni@students.isatu.edu.ph>
2025-04-20 10:42:58 +0800

    Create dashboard.dart

---
+++ lib/pages/dashboard/dashboard.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:voting_app/pages/header-image.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(child: Column(children: [HeaderImage()]));
+  }
+}
